ed23976ed58d1f57300ca1951b330f7057e33200
Harrison <hsfisher@email.neit.edu>
2026-04-07 08:18:15 -0400

    Ball movement

--- ball.js
+++ ball.js
@@ -1,22 +1,20 @@
 // Ball Function
 function Ball() {
-    // Ball Properties
     this.x = 400;
     this.y = 250;
     this.width = 50;
     this.height = 50;
     this.color = "purple";
     this.force = 0;
-    this.vx = 0;
+    this.vx = 2; // horizontal speed
     this.vy = 0;
 
-    // Draw Method
     this.draw = function() {
         ctx.beginPath();
         ctx.arc(
-            this.x,                // x position
-            this.y,                // y position
-            this.width / 2,        // radius
+            this.x,
+            this.y,
+            this.width / 2,
             0,
             360 * Math.PI / 180,
             true
@@ -26,7 +24,6 @@ function Ball() {
         ctx.fill();
     };
 
-    // Move Method
     this.move = function() {
         this.x += this.vx;
         this.y += this.vy;
@@ -40,5 +37,14 @@ var ctx = canvas.getContext("2d");
 // Create Ball
 var ball = new Ball();
 
-// Draw Ball
-ball.draw();
+// Game loop
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ball.move();
+    ball.draw();
+
+    requestAnimationFrame(update);
+}
+
+update();
